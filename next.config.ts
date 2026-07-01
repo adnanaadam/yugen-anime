0264b9204aa485d18b312fe87fa367e716850c8f
sharawi <adnanmish30@gmail.com>
2026-07-01 23:53:56 +0000

    update docs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,71 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+    remotePatterns: [
+       {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+      protocol: 'https',
+      hostname: '**.anilist.co',  // This allows any subdomain of anilist.co
+      port: '',
+      pathname: '/**',
+    },
+     {
+      protocol: 'https',
+      hostname: 'cdn.myanimelist.net',
+      port: '',
+      pathname: '/**',
+    },
+    ],
+  },
+  
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              icon: true,
+              replaceAttrValues: {
+                '#000': 'currentColor',
+                '#000000': 'currentColor',
+                'black': 'currentColor',
+                '#fff': 'currentColor', 
+                '#ffffff': 'currentColor',
+                'white': 'currentColor',
+                '#e5b23c': 'currentColor',
+                '#ff5b47': 'currentColor',
+                '#d8d5cc': 'currentColor',
+                '#6C5CE7': 'currentColor',
+              },
+              svgProps: {
+                fill: 'currentColor',
+                stroke: 'currentColor',
+              },
+            },
+          },
+        ],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 export default nextConfig;
